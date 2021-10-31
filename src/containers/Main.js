cb0b478cb7acfa47f7673957f00e1615bc13b3df
Christopher Dent <christopherdent01@gmail.com>
2021-10-31 01:23:39 -0400

    you did it, you crazy son of a bitch you did it

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 
 import React, { useState, useEffect } from 'react'
-
+import Tile from '../components/Tile'
 import Grid from '../components/Grid'
 import Heading from '../components/Heading'
 import Filter from '../components/Filter'
@@ -18,12 +18,13 @@ class Main extends React.Component {
 
   constructor() {
     super();
-    // this.onCheckBoxChanged = this.onCheckBoxChanged.bind(this);
+    
     this.state = {
-      airlines: []
-    }
+      airlines: [],
+      filter: 'none',
+      checked: false
   }
-  
+} 
   
 
   fetchAirlines = () => {
@@ -31,14 +32,11 @@ class Main extends React.Component {
       jsonpCallback: "jsonp"
     })
     .then(res => res.json())
-    // .then(({data}) => {
-    //   console.log(data )
-    //   this.setState({ airlines: data })  
+     
     .then(json => {
-      json.forEach(airline => console.log(JSON.stringify(airline.name)));
-      this.setState({airlines : json})
-      // json.forEach(airline => allAirlines.push(airline));
-          
+      // json.forEach(airline => console.log(JSON.stringify(airline.name)));
+      this.setState({airlines : json, checked: false})
+      console.log(this.state)
     })
   }
 
@@ -48,46 +46,164 @@ class Main extends React.Component {
       this.fetchAirlines()
     }
 
-
-    onCheckBoxChanged = (event) => {
-        
-      // Just to show an indication that I got in the function
-      console.log("i got clicked");
-    }
-  
-
-    handleClick = () => {
-
-      const airlineArray = this.state.airlines 
-      let filteredAirlines = airlineArray.filter(airline => airline.name === "Delta")
-
-      this.setState({
-        airlines: filteredAirlines
-      })
-
-      console.log('test')
+    handleCheckbox = e => {
+      if (this.state.filter === e.target.value) this.setState({ filter: "none" });
+      else this.setState({ filter: e.target.value, checked: true });
+    };
     
-    }
 
+    // handleClick = () => {
 
+    //   const airlineArray = this.state.airlines 
+    //   let filteredAirlines = airlineArray.filter(airline => airline.name === "Delta")
+
+    //   this.setState({
+    //     airlines: filteredAirlines
+    //   })
+
+    //   console.log('test')
+    
+    // }
+
+    //   skyteam = () => {
+    //     const airlineArray = this.state.airlines 
+    //     let filteredAirlines = airlineArray.filter(airline => airline.alliance === "ST")
+
+    //     this.setState({
+    //       airlines: filteredAirlines, checked: true 
+    //     })
+    // }
+
+    //   oneworld = () => {
+    //     const airlineArray = this.state.airlines 
+    //     let filteredAirlines = airlineArray.filter(airline => airline.alliance === "OW")
+
+    //     this.setState({
+    //       airlines: filteredAirlines
+    //     })
+    // }
+
+    //   staralliance = () => {
+    //     const airlineArray = this.state.airlines 
+    //     let filteredAirlines = airlineArray.filter(airline => airline.alliance === "SA")
+
+    //     this.setState({
+    //       airlines: filteredAirlines
+    //     })
+    //   }
+
+  //   handleChange = event => {
+  //    debugger 
+  //     const item = event.target.name;
+  //     const isChecked = e.target.checked;
+  //     this.setState(prevState => ({
+  //         checkedItems: prevState.checkedItems.set(item, isChecked)
+  //     }));
+  // };
+
+    //unify all checkboxes to share logic, e holds information about event that triggered this function, we will add value to each checkbox for easy access
+    handleCheckbox = e => {
+       
+      if (this.state.filter === e.target.value) this.setState({ filter: "none" });
+
+      else this.setState({ filter: e.target.value, checked: true });
+       
+    };
+
+  
   render(){   
-     
-  return (
+       //mapped variable will handle filtering results, if `state.filter` matches `item.type` item will be rendered, also if filter is set to none, all items are rendered, this will not preserve multiple filters though,
+      const filterList = this.state.airlines.map(airline => {
 
+          if (airline.alliance === this.state.filter || this.state.filter === "none")
+             
+          return (
+            
+            <Col sm={6} md={4} lg={3}>
+            <Tile 
+              key = {airline.code}
+              logo = {airline.logoURL}
+              name = {airline.name}
+              alliance = {airline.alliance}
+            />
+         </Col>
+         
+        );
+      });
+    
+  
+  return (
+ 
     <>    
         <Container>
           <Heading />
-          <Button onClick = {this.handleClick}> Tester </Button> 
-    
-            <Form.Group className="mb-3" controlId="formBasicCheckbox" id="inputs">
-            <Form.Check type="checkbox" label="Oneworld" id= "oneworld" onClick = {this.onCheckBoxChanged}/>
-            <Form.Check type="checkbox" label="Sky Team" id = "skyteam"  onClick = {this.onCheckBoxChanged}  />
-            <Form.Check type="checkbox" label="Star Alliance" id = "staralliance" onClick = {this.onCheckBoxChanged} />
-            </Form.Group>
+        
+          {/* <form>
+          <label htmlFor="myInput">All</label>
+          <input
+            id="myInput"
+            value="All"
+            type="checkbox"
+            onClick={this.handleCheckbox}
+          />
+          <label htmlFor="myInput">Sky Team</label>
+          <input
+            id="myInput"
+            type="checkbox"
+            value="ST"
+            onClick={this.handleCheckbox}
+          />
+          <label htmlFor="myInput">One World</label>
+          <input
+            id="myInput"
+            value="OW"
+            type="checkbox"
+            onClick={this.handleCheckbox}
+          />
+          <label htmlFor="myInput">Star Alliance</label>
+          <input
+            id="myInput"
+            value="SA"
+            type="checkbox"
+            onClick={this.handleCheckbox}
+          />
+        </form> */}
+          <Form.Group className="mb-3" controlId="formBasicCheckbox" id="inputs" className="alliance-list">
           
+     
 
-       
-       
+          <Form.Check 
+             
+            type="checkbox" 
+            label="One World" 
+            id= "One World" 
+            onClick={this.handleCheckbox}
+           
+            value="OW"
+            />
+          <Form.Check 
+           
+          type="checkbox" 
+          label="Sky Team" 
+          id = "Sky Team"  
+          onClick={this.handleCheckbox}
+         
+          value="ST"
+          />
+          <Form.Check 
+          type="checkbox" 
+          label="Star Alliance" 
+          id = "Star Alliance"  
+          onClick={this.handleCheckbox}
+          value="SA"
+          />
+           
+      </Form.Group>
+
+       <Row>
+         {filterList}
+      </Row>
+          
           {/* <Filter 
           airlines = { this.state.airlines }
           onCheckBoxChanged = { event => console.log(event) }
@@ -95,11 +211,11 @@ class Main extends React.Component {
           /> */}
 
 
-          <Grid 
+          {/* <Grid 
             airlines = { this.state.airlines }
             tests = { 'Im a little tea prop' }
             
-            />
+            /> */}
         </Container>
     </>
     );
