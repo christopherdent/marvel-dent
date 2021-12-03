e061fafbbc2b1c2d5f8b6e81fb3a4c40f4f7ee07
Christopher Dent <christopherdent01@gmail.com>
2021-12-03 12:41:43 -0500

    revert to earlier commit

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -80,16 +80,13 @@ class Main extends React.Component {
  
  
       const filterList = this.state.comics.map(comic => {          
-         
+        if (comic.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
         return (  
           <Col sm={6} md={4} lg={4}>
            <Tile 
             key = {comic.id}      
-            title = {comic.title}    
-            
-            image = { comic.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available' ? comic.thumbnail.path + "." + comic.thumbnail.extension : 'https://i5.walmartimages.com/asr/7c39143f-6d73-4f81-8c4e-9418804f47f2_1.0a075aa7294b8a62fe99895a82618550.jpeg' }
-
-           
+            title = {comic.title}       
+            image = { comic.thumbnail.path + "." + comic.thumbnail.extension }
             creators = { comic.creators.items[0] ? comic.creators.items[0].name + " et al." : null }
             moreinfo = { comic.urls[0].url}
     
@@ -97,7 +94,21 @@ class Main extends React.Component {
             />
          </Col>         
         );
-    
+        } else {
+          return (  
+          <Col sm={6} md={4} lg={4}>
+          <Tile 
+           key = {comic.id}      
+           title = {comic.title}       
+           image = 'https://i5.walmartimages.com/asr/7c39143f-6d73-4f81-8c4e-9418804f47f2_1.0a075aa7294b8a62fe99895a82618550.jpeg'
+           creators = { comic.creators.items[0] ? comic.creators.items[0].name + " et al." : null }
+           moreinfo = { comic.urls[0].url}
+   
+         
+           />
+        </Col>    
+        )
+       }
       }
      )
   
