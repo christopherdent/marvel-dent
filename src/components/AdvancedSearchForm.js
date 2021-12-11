f845eb82351f11ba4adf6c01b2564c8687fc40ad
Christopher Dent <christopherdent01@gmail.com>
2021-12-11 17:37:05 -0500

    add prop for submtting adv search form

--- src/components/AdvancedSearchForm.js
+++ src/components/AdvancedSearchForm.js
@@ -8,7 +8,9 @@ import Button from 'react-bootstrap/Button'
 function AdvancedSearchForm(props) {
    
   function handleClose() {
-    props.setShow(false);
+    props.onSubmit();   ///runs search 
+    props.setShow(false);   ///closes modal 
+    
   } 
   
 
@@ -19,15 +21,28 @@ function AdvancedSearchForm(props) {
     <center>
 
     <p>Advanced Search Coming Soon!</p>
-    
-    <Button variant="secondary" onClick={handleClose}>
+        <Form onSubmit={props.onSubmit}>
+           <div className="form-group">
+          <label htmlFor="name">Name</label>
+           <input className="form-control" id="name" />
+        </div> 
+      
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input type="email" className="form-control" id="email"
+        placeholder="name@example.com" 
+        />
+    </div>
+
+
+              <Button variant="secondary" onClick={handleClose}>
                 Close
               </Button>
               
-              <Button variant="danger" onClick={handleClose} id="advancedSearchButton" disabled>
+              <Button variant="danger" onClick={handleClose} id="advancedSearchButton">
                 Search Marvel 
               </Button>
-         
+         </Form>
             
             </center>
     </>
